a3a38b530d990bbb4d346fcbfa42322525f47f96
Jitendrap1702 <56450901+Jitendrap1702@users.noreply.github.com>
2020-09-06 20:41:10 +0530

    Create palindrome.py

---
+++ Functions/palindrome.py
@@ -0,0 +1,14 @@
+def checkPalindrome(num):
+    k=str(num)
+    if k==k[::-1]:
+        return True
+    else:
+        return False
+
+		
+num = int(input())
+isPalindrome = checkPalindrome(num)
+if(isPalindrome):
+	print('true')
+else:
+	print('false')

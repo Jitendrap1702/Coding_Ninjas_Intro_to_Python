0393e4e461c56646ea4b0e23f00db9f9002a1c0e
Jitendrap1702 <56450901+Jitendrap1702@users.noreply.github.com>
2020-09-06 20:39:26 +0530

    Create forenhiteToCelcius.py

---
+++ Functions/forenhiteToCelcius.py
@@ -0,0 +1,11 @@
+def printTable(start,end,step):
+    for i in range(start,end+1,step):
+        print(i,' ',int(((i-32)*5)/9))
+
+
+	   
+s = int(input())
+e = int(input())
+step = int(input())
+printTable(s,e,step)
+

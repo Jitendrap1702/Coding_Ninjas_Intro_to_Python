20f1dcd2d309445360cb9bcc1d37b30b36e08067
Jitendrap1702 <56450901+Jitendrap1702@users.noreply.github.com>
2020-09-06 18:03:57 +0530

    Add files via upload

---
+++ fibonacci_seq.py
@@ -0,0 +1,17 @@
+n=int(input("enter a number"))
+def fibonacci_seq(n):
+    a=0
+    b=1
+    if n==1:
+        print(a)
+    elif n==2:
+        print(a,b)
+    else:
+        print(a,b,end=" ")
+        for _ in range(n-2):
+            c=a+b
+            a=b
+            b=c
+            print(b,end=" ")
+
+fibonacci_seq(n)
